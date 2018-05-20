8afb11b52716ae2e0896a7317a31d8dc392646c0
kadenjenson <sobeit99@gmail.com>
2018-05-20 00:18:35 -0600

    Started testing seeds data. Terrible card styles rn

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+User.create!(
+	email: "admin@test.com",
+	password: "asdfasdf",
+	password_confirmation: "asdfasdf",
+	name: "Kaden Jenson"
+	)
+
+puts "One user created."
+
 20.times do |blog|
 	Blog.create!(
 		title: "testing seeds #{blog}",
@@ -11,3 +20,18 @@
 end
 
 puts "20 Blogs created."
+
+10.times do |portfolio|
+	Portfolio.create!(
+		title: "Test data #{portfolio}",
+		body: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, 
+    eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+    voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque
+    porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
+    incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam 
+    corporis suscipit laboriosam",
+    thumb_img: "coding_large_0.jpg"
+		)
+end
+
+puts "10 Portfoilo items created."
